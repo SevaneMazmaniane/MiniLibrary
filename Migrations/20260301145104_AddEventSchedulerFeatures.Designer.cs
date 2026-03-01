376ca6d94f6777ca3e528fd593e5a8ff4d05cc24
SevaneM <Sevane.Mazmaniane-e@maf.ae>
2026-03-01 19:06:40 +0400

    Fix migration

--- Migrations/20260301145104_AddEventSchedulerFeatures.Designer.cs
+++ Migrations/20260301145104_AddEventSchedulerFeatures.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiniLibrary.Data;
 
@@ -10,9 +11,11 @@ using MiniLibrary.Data;
 namespace MiniLibrary.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260301145104_AddEventSchedulerFeatures")]
+    partial class AddEventSchedulerFeatures
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
